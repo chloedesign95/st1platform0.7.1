60e3fdf5a425b5ea93f65986b86725691e6d733b
Chloe <chloedesign95@gmail.com>
2023-01-31 17:26:00 +0900

    #0. 설정과 시작부분 주석 내용 추가

--- src/main/java/com/st1/platform/domain/Article.java
+++ src/main/java/com/st1/platform/domain/Article.java
@@ -30,7 +30,7 @@ public class Article extends AuditingFields {
     private UserAccount userAccount; // 유저 정보 (ID)
 
     @Setter @Column(nullable = false) private String title; // 제목
-    @Setter @Column(nullable = false, length = 255) private String content; // 본문
+    @Setter @Column(nullable = false, length = 10000) private String content; // 본문
 
     @ToString.Exclude
     @JoinTable(
